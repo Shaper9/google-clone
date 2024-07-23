2de15a9c39e2ac909c230850500d6a5fb50e13ef
Shaper9 <marko.krstin9@gmail.com>
2024-07-23 14:54:53 +0200

    remove-type

--- src/components/WebSearchResults.tsx
+++ src/components/WebSearchResults.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import Parser from "html-react-parser";
 
-export default function WebSearchResults({ results }: any) {
+export default function WebSearchResults({ results }) {
   return (
     <div className="w-full mx-auto px-3 sm:pb-24 pb-40 sm:pl-[5%] md:pl-[14%] lg:pl-52">
       <p className="text-gray-600 text-sm mb-5 mt-3">
